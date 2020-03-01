d624238a90eeb0a49cd296bf552ebe6c9c50970b
Sarah Eastwood <eastwoos@oregonstate.edu>
2020-02-29 20:53:35 -0800

    Fixed blank line error in tests.py

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 import task
 
 class TestCase(unittest.TestCase):
+
     def test1(self):
         expected = "success"
         self.assertEqual(expected, task.firstrun())
@@ -9,6 +10,7 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
 
 if __name__ == '__main__':
     unittest.main()
